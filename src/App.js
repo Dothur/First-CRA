d272372c6c67bb21cb24403f5f10dbfb0e00634d
dohunk <craz1000@naver.com>
2023-03-14 00:10:50 +0900

    Study useEffect !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("call the api...");
+    // 맨 처음 단 한번만 실행됨!
+  }, []);
+  //useEffect 를 사용하는 이유!
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button text={"Continue!"} />
     </div>
   );
 }
