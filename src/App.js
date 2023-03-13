3bb9cdcb0ce2d37a7edc07a9464d6fc9f6c23da1
dohunk <craz1000@naver.com>
2023-03-14 00:27:33 +0900

    Study useEffect part2..

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
     console.log("call the api...");
     // 맨 처음 단 한번만 실행됨!
   }, []);
   //useEffect 를 사용하는 이유!
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       <Button text={"Continue!"} />
