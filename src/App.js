93e7d818c4c6256ed7dbebb3508a82e5105e4c42
dohunk <craz1000@naver.com>
2023-03-15 23:20:11 +0900

    Challenge make Coin tracker. / input money 로 코인을 얼마나 살 수 있는지를 만들었다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
+  const onChange = (event) => setMyMoney(event.target.value);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,17 +15,27 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `${coins.length}`}</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <div>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}) : {myMoney / coin.quotes.USD.price}{" "}
+                {coin.symbol}
+              </option>
+            ))}
+          </select>
+          <hr />
+          <input
+            onChange={onChange}
+            value={myMoney}
+            type="text"
+            placeholder="Write your Money(USD)"
+          />
+        </div>
       )}
     </div>
   );
