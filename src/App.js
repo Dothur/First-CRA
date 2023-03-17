b783d22031efa4e245eb1e05802734a1b26f8035
dohunk <craz1000@naver.com>
2023-03-17 13:18:38 +0900

    Install react-router-dom@5.3.0 , Study Movie app part2 ..

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-    // .then 을 사용하지않고 async await 문을 사용함 형태 다시 공부하기
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              {movie.medium_cover_image == null ? null : (
-                <img src={movie.medium_cover_image} />
-              )}
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
