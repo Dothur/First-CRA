d7b4ba6f650fffce0c0749827b26875cf75d985e
dohunk <craz1000@naver.com>
2023-03-17 11:57:42 +0900

    Study Movie app part1.

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState([]);
-  const onChange = (event) => setMyMoney(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+    // .then 을 사용하지않고 async await 문을 사용함 형태 다시 공부하기
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
+
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <select>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}) : {myMoney / coin.quotes.USD.price}{" "}
-                {coin.symbol}
-              </option>
-            ))}
-          </select>
-          <hr />
-          <input
-            onChange={onChange}
-            value={myMoney}
-            type="text"
-            placeholder="Write your Money(USD)"
-          />
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              {movie.medium_cover_image == null ? null : (
+                <img src={movie.medium_cover_image} />
+              )}
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
     </div>
